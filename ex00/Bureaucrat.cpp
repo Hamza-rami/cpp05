c0a6ca5d714480b35ac44187406cdae3c9459040
Hamza-rami <ramihamza504@gmail.com>
2026-02-10 11:30:46 +0100

    finnished ex00

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,67 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name("Default"), grade(150)
+{}
+
+Bureaucrat::~Bureaucrat()
+{}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : name(name), grade(grade)
+{
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    else if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : name(other.name), grade(other.grade)
+{}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+    if (this != &other)
+    {
+        this->grade = other.grade;
+    }
+    return *this;
+}
+
+std::string Bureaucrat::getName() const
+{
+    return this->name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return this->grade;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade too High";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade too Low";
+}
+
+void Bureaucrat::increment()
+{
+    grade++;
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+}
+
+void Bureaucrat::decrement()
+{
+    grade--;
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+}
+
+std::ostream& operator<<(std::ostream &out, const Bureaucrat& b)
+{
+    out << b.getName() << " ,bureaucrat grade " << b.getGrade();
+    return out;
+}
